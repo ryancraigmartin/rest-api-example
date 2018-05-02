84a81130cab91b0d4ddc27f15ebe41b4a8d36fe7
Ryan Martin <rmart106@fiu.edu>
2018-05-02 15:52:27 -0400

    Simple REST API

--- app.js
+++ app.js
@@ -8,7 +8,27 @@ const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const Schema       = mongoose.Schema;
 
+const citySchema = Schema({
+  name:  String,
+  id: Number
+});
+const City = mongoose.model("City", citySchema);
+
+const courseSchema = Schema({
+  name:  String,
+  id: Number,
+  type: String
+});
+const Course = mongoose.model("Course", courseSchema);
+
+const bootcampSchema = Schema({
+  id: Number,
+  cityId: Number,
+  courseId: Number
+});
+const Bootcamp = mongoose.model("Bootcamp", bootcampSchema);
 
 mongoose.Promise = Promise;
 mongoose
@@ -31,28 +51,33 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Express View engine setup
-
 app.use(require('node-sass-middleware')({
   src:  path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
       
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
+app.get("/cities", (req,res,next) =>{
+  City.find((err, cities) => {
+    res.json(cities) // Generates the cities as a JSON file.
+  });
+});
 
+app.get("/cities/:id", (req,res,next) => {
+  City.findOne({id: req.params.id}, (err, theCity) => {
+    res.json(theCity) // Generates the city selected by ID as a JSON file.
+  })
+});
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
 
-
-
 const index = require('./routes/index');
 app.use('/', index);
-
 
 module.exports = app;
